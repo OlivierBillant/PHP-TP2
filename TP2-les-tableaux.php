2e24e27c16bd9c5d6f79dd48a06e061a972a8f75
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-17 09:18:59 +0200

    CREATE

---
+++ TP2-les-tableaux.php
@@ -0,0 +1,16 @@
+<?php
+//Excercice 1
+$val_Antoine[] = "prenom";
+$val_Antoine[] = "ville";
+$val_Antoine[] = "age";
+
+$val_Phileas[] = "prenom";
+$val_Phileas[] = "ville";
+$val_Phileas[] = "age";
+
+$annuaire["Guillou"] = $val_Antoine;
+$annuaire["Peron"] = $val_Phileas;
+
+var_dump($annuaire);
+
+//Excercice 2
